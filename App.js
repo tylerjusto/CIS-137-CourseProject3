c9e463383c30d88572b48e12be354967b0152e26
tylerjusto <tbjusto2014@gmail.com>
2017-07-21 00:01:42 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import {BrowserRouter, Route, Link} from 'react-router-dom'
+import './App.css';
+import Home from './Home';
+import About from './About';
+import Events from './Events';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            headerMinimized: false
+        };
+    }
+
+    minimizeHeader = () => {
+        this.setState({
+            headerMinimized: !this.state.headerMinimized
+        });
+    };
+
+    render() {
+        let {headerMinimized} = this.state;
+        return (
+			<BrowserRouter>
+				<div>
+					<div className={`App-hero ${headerMinimized ? 'minimized' : ''}`}>
+						<header onClick={this.minimizeHeader}>
+							<Link to="/"><h1>Tanning Oasis {process.env.REACT_APP_WEATHER_ACCESS_KEY}</h1></Link>
+						</header>
+						<nav>
+							<Link to="/">Home</Link> |
+							<Link to="/About">About</Link> |
+							<Link to="/Events">Events</Link>
+						</nav>
+					</div>
+					<Route path="/" exact component={Home}/>
+					<Route path="/About" exact component={About}/>
+					<Route path="/Events" exact component={Events}/>
+				</div>
+			</BrowserRouter>
+        );
+    }
+}
+
+
+
+
+
+export default App;
